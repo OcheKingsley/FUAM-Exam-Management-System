491637ce745e082cda6a6d3fbe5d66e7c6f79604
ochekingsley <kingsleyoche072@gmail.com>
2026-07-12 13:19:02 +0100

    Load home page as default route

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+app.use(express.static(path.join(__dirname, 'public'), { index: false })); // Serve static files
 
 
 // ===== MySQL Connection =====
